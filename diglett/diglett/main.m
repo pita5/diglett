ccc876d3df024f300f3192f78237e1282c4595d2
Alex Gordon <anything@fileability.net>
2011-09-28 23:32:14 +0100

    Added project

---
+++ diglett/diglett/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  diglett
+//
+//  Created by Alex Gordon on 28/09/2011.
+//  Copyright 2011 Fileability. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
